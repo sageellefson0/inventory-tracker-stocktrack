992e3d34c19bb2030453398c9683ceee7b75faff
Sage Ellefson <sagecole0@gmail.com>
2024-07-31 15:15:32 -0500

    removed anayltics code in attempt to deploy

--- inventory-tracker-stocktrack/firebase.js
+++ inventory-tracker-stocktrack/firebase.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "firebase/firestore";
 // import env from "dotenv";
 // env.config();
@@ -22,7 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app)
-const analytics = getAnalytics(app);
 export{app, firestore}
 
 
