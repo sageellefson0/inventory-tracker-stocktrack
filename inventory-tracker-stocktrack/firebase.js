d08cba59b0f53ee0d9d7655206972265a2c18896
Sage Ellefson <sagecole0@gmail.com>
2024-07-31 16:13:00 -0500

    adjusted config files

--- inventory-tracker-stocktrack/firebase.js
+++ inventory-tracker-stocktrack/firebase.js
@@ -20,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firestore = getFirestore(app)
-export{app, firestore}
+const firestore = getFirestore(app);
+export{app, firestore};
 
 
